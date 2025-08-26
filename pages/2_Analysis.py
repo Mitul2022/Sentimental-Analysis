759857602277165fc458130a2e877736ea5b66b9
Mitul Luhar <96614769+Mitul2022@users.noreply.github.com>
2025-08-26 12:12:59 +0530

    Update 2_Analysis.py

--- pages/2_Analysis.py
+++ pages/2_Analysis.py
@@ -267,3 +267,4 @@ if st.session_state.processed_data is not None:
     st.download_button("📥 Download Full Aspect Data",
                        st.session_state.processed_data.to_csv(index=False).encode("utf-8"),
                        "aspect_level_breakdown.csv","text/csv")
+
